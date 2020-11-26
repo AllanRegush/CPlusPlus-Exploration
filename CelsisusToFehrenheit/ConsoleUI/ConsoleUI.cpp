f18098dd6be97dc1ff0d3296e3e735c2a42fbd7d
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-11-25 19:08:08 -0600

    C Style Temp Converter

---
+++ CelsisusToFehrenheit/ConsoleUI/ConsoleUI.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+    float fahr, celsius;
+    int lower = 0, upper = 300, step = 20;
+    fahr = lower;
+    std::printf("Celsius | Fahrenheit\n");
+    std::printf("--------------------\n");
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        std::printf("%6.1f %6.1f\n", celsius, fahr);
+        fahr = fahr + step;
+    }
+}
+
